9bd1a76859c1cd18b4a6e9b8310b2dd58a70605d
Stepan Redka <cogitofallacy@gmail.com>
2019-11-18 03:25:34 +0300

    fix: common vocabulary using undead languageId

--- vocabularies/undead.lua
+++ vocabularies/undead.lua
@@ -1,3 +1,6 @@
 -- Lorderon humans speak the same language as Stormwind humans, but they choose to deny it
-HERMES_UNDEAD = HERMES_COMMON
+HERMES_UNDEAD = {}
+for k,v in pairs(HERMES_COMMON) do
+    HERMES_UNDEAD[k] = v
+end
 HERMES_UNDEAD['LanguageID'] = 33
